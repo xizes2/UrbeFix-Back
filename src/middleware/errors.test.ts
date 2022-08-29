96eb4be012feff3610f7aa2af6c5dfa4232c8578
Fernando Paulino <xizes2@hotmail.com>
2022-08-29 20:25:27 +0200

    create custom error, error interface and test

---
+++ src/middleware/errors.test.ts
@@ -0,0 +1,54 @@
+import { NextFunction, Request, Response } from "express";
+import ICustomError from "../interfaces/ICustomError";
+import { generalError, notFoundError } from "./errors";
+
+describe("Given an notFoundError function", () => {
+  describe("When its called", () => {
+    test("Then it should respond with status 404 and {error: 'Endpoint not found' }", async () => {
+      const req = {};
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockResolvedValue({ error: "Endpoint not found" }),
+      };
+      const status = 404;
+      const resolvedJson = { error: "Endpoint not found" };
+
+      await notFoundError(
+        req as unknown as Request,
+        res as unknown as Response
+      );
+
+      expect(res.status).toBeCalledWith(status);
+      expect(res.json).toBeCalledWith(resolvedJson);
+    });
+  });
+});
+
+describe("Given an generalError function", () => {
+  describe("When its called", () => {
+    test("Then it should respond with status the received error code and {error: errorMessage}", async () => {
+      const error = {
+        code: 666,
+        publicMessage: "Total error, hell broke loose!",
+      };
+      const req = {};
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockResolvedValue(error.publicMessage),
+      };
+      const next = jest.fn;
+      const status = 666;
+      const resolvedJson = { error: error.publicMessage };
+
+      await generalError(
+        error as ICustomError,
+        req as unknown as Request,
+        res as unknown as Response,
+        next as NextFunction
+      );
+
+      expect(res.status).toBeCalledWith(status);
+      expect(res.json).toBeCalledWith(resolvedJson);
+    });
+  });
+});
---
+++ src/middleware/errors.ts
@@ -0,0 +1,24 @@
+import chalk from "chalk";
+import debug from "debug";
+import { NextFunction, Request, Response } from "express";
+import ICustomError from "../interfaces/ICustomError";
+
+export const notFoundError = (req: Request, res: Response) => {
+  res.status(404).json({ error: "Endpoint not found" });
+};
+
+export const generalError = (
+  error: ICustomError,
+  req: Request,
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  next: NextFunction
+) => {
+  const errorCode = error.code ?? 500;
+  const errorMessage =
+    error.publicMessage ?? "Something went wrong, please try again";
+
+  debug(chalk.bgRedBright(`Error code: ${errorCode} => ${errorMessage}`));
+
+  res.status(errorCode).json({ error: errorMessage });
+};

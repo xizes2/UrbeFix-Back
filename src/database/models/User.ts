46182564a8a80882e87ba47d2ecbff9c4d3d8aee
Fernando Paulino <xizes2@hotmail.com>
2022-08-31 20:07:53 +0200

    correct data names

--- src/database/models/User.ts
+++ src/database/models/User.ts
@@ -1,11 +1,11 @@
 import { model, Schema } from "mongoose";
 
 const userSchema = new Schema({
-  firstName: { type: String, required: true, unique: true },
-  lastName: { type: String, required: true, unique: true },
-  profileImage: { type: String, unique: true },
+  firstName: { type: String, required: true },
+  firstSurname: { type: String, required: true },
+  profileImage: { type: String },
   userEmail: { type: String, required: true, unique: true },
-  password: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
 export const User = model("User", userSchema, "users");

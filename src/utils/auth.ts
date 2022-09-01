eb4b9a50a2a2edfd95ba09c59bfb97ddded3229a
Fernando Paulino <xizes2@hotmail.com>
2022-09-01 21:20:04 +0200

    create login endpoint, change validation params, create hash functions

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,6 +1,12 @@
 import "../loadEnvironments";
 import bcrypt from "bcryptjs";
+import ICustomJwtPayload from "../interfaces/ICustomJwtPayload";
+import jwt from "jsonwebtoken";
 
-const hashCreator = (text: string) => bcrypt.hash(text, 10);
+export const hashCreator = (text: string) => bcrypt.hash(text, 10);
 
-export default hashCreator;
+export const hashCompare = (text: string, hash: string) =>
+  bcrypt.compare(text, hash);
+
+export const createToken = (payload: ICustomJwtPayload) =>
+  jwt.sign(payload, process.env.SECRET);
